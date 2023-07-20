35033c1bcc51146b646a965347ee018dec3f42b5
Aderynlola <125492983+Aderynlola@users.noreply.github.com>
2023-07-20 10:33:28 +0100

    Correct correction on task 4

--- python-functions/4-fibonacci.py
+++ python-functions/4-fibonacci.py
@@ -1,10 +1,12 @@
 def fibonacci_sequence(n):
     if n <= 0:
         return []
-    
+    elif n == 1:
+        return [0]
+
     list = [0, 1]
     while len(list) < n:
         nextnumber = list[-1] + list[-2]
         list.append(nextnumber)
-    
+
     return list
