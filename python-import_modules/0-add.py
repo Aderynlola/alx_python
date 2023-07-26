cc6d2af9bfec25e22718605c81de2849a377551b
Aderynlola <125492983+Aderynlola@users.noreply.github.com>
2023-07-26 15:16:48 +0100

    Task 0

--- python-import_modules/0-add.py
+++ python-import_modules/0-add.py
@@ -1,19 +1,9 @@
+from add_0 import add
+
 a = 1;
 b = 2;
-
-def add(a, b):
-    """My addition function
-
-    Args:
-        a: first integer
-        b: second integer
-
-    Returns:
-        The return value. a + b
-    """
-    return (a + b)
-
-if --__name__ == "__main__":
-    c = add(a, b);
-
+c = add(a, b);
+   
 print("{} + {} = {}" .format(a, b, c))
+if __name__ == "__main__":
+    pass
