6022a135638bb226bb9397ef704c4eab215a0f98
Aderynlola <125492983+Aderynlola@users.noreply.github.com>
2023-08-01 18:37:11 +0100

    task 0

--- python-classes/0-square.py
+++ python-classes/0-square.py
@@ -1,4 +1,15 @@
+"""Square class definition"""
 class Square:
 
+    """Represents a square
+    Attributes:
+        __size (int): size of a side of the square
+    """
+
     def __init__(self, size):
+        """Initializes the attributes of the square
+        Args:
+            size (int): size of a side of the square
+        Returns: None
+        """
         self.__size = size
