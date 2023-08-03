753d7f5b954b3d0f9905c09f6d8ba6587439ccd7
Aderynlola <125492983+Aderynlola@users.noreply.github.com>
2023-08-03 10:59:53 +0100

    Task 8

--- python-inheritance/8-square.py
+++ python-inheritance/8-square.py
@@ -1,90 +1,31 @@
-"""defining a class BaseGeometry"""
-class BaseGeometry:
-    """a base class for geometrical operations"""
-    def area(self):
-        """calculates area of the geometry
-        raises Exception with the message "area() is not implemented"
-        """
-        raise Exception("area() is not implemented")
-    
-    def integer_validator(self, name, value):
-        """
-        Validate if the value is an integer greater than 0.
 
-        name (str): The name of the value being validated.
-        value (int): The value to be validated.
+"""Creates a Square class."""
+Rectangle = __import__('7-rectangle').Rectangle
 
-        Raises:
-            TypeError: If value is not an integer.
-            ValueError: If value is less or equal to 0.
-        """
-        if not isinstance(value, int):
-            raise TypeError("{} must be an integer" .format(name))
-        if value <= 0:
-            raise ValueError("{} must be greater than 0" .format(name))
-        
-class Rectangle(BaseGeometry):
-    """
-    class Rectangle inherits from class BaseGeometry.
-
-    __width (int): The width of the rectangle.
-    __height (int): The height of the rectangle.
-
-    __init__(self, width, height): Initializes a Rectangle object with the specified width and height.
-    __str__(self): Returns a string representation of the Rectangle object.
-    """
-    def __init__(self, width, height):
-        """
-        Initialize a Rectangle object with the specified width and height.
-
-        width (int): The width of the rectangle.
-        height (int): The height of the rectangle.
-
-        Raises:
-            TypeError: If width or height is not an integer.
-            ValueError: If width or height is less or equal to 0.
-        """
-        self.__width = width
-        self.__height = height
-        self.integer_validator("width", self.__width)
-        self.integer_validator("height", self.__height)
-
-    def __str__(self):
-        """
-        Returns a string representation of the Rectangle object.
-
-        Returns a string containing the width and height of the rectangle.
-        """
-        return f"[Rectangle] {self.__width}/{self.__height}"
-    
-    def area(self):
-        """
-        Calculates the area of the rectangle.
-
-        Returns the area of the rectangle.
-        """
-        return self.__width * self.__height
-    
 class Square(Rectangle):
-    """
-    class Square,inherits from class Rectangle.
-
-    Attributes:
-        __size (int): The size of the square.
-
-    Methods:
-        __init__(self, size): Initializes a Square object with the specified size.
+    """class Square that inherits from Rectangle (9-rectangle.py)
+    Private instance attribute size.
+    Public method area().
+    Inherits from Rectangle.
     """
 
     def __init__(self, size):
+        """Initializes a Square.
+        Args:
+            - size: size of the square
         """
-        Initialize a Square object with the specified size.
 
-        size (int): The size of the square.
-
-        Raises:
-            TypeError: If size is not an integer.
-            ValueError: If size is less or equal to 0.
-        """
+        self.integer_validator("size", size)
         super().__init__(size, size)
+        self.__size = size
 
+    def __str__(self):
+        return super().__str__()
+
+    def area(self):
+        """Computes the area of a Square instance.
+        Overwrites the area() method from Rectangle.
+        """
+
+        return self.__size ** 2
+    
