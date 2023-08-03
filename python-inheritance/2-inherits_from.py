134c04cfa8aa7887bf943229357ca03fc3c925da
Aderynlola <125492983+Aderynlola@users.noreply.github.com>
2023-08-03 06:30:31 +0100

    Task 2

--- python-inheritance/2-inherits_from.py
+++ python-inheritance/2-inherits_from.py
@@ -1,10 +1,17 @@
-"""
-a function defining an object that inherits from the specified class
-"""
+#!/usr/bin/python3
+"""Defines an inherited class-checking function."""
+
+
 def inherits_from(obj, a_class):
+    """Checks if an object is an inherited instance of a class.
+
+    Args:
+        obj (any): The object to check.
+        a_class (type): The class to match the type of obj to.
+    Returns:
+        If obj is an inherited instance of a_class - True.
+        Otherwise - False.
     """
-    returns True if the object is an instance of a class 
-    that inherited (directly or indirectly) from the specified class 
-    otherwise False
-    """
-    return isinstance(obj, type) and issubclass(type(obj), a_class)
+    if issubclass(type(obj), a_class) and type(obj) != a_class:
+        return True
+    return False
