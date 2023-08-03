7af3c864f8fc538f5e64aa7e95e4360d15594697
Aderynlola <125492983+Aderynlola@users.noreply.github.com>
2023-08-03 10:53:07 +0100

    Task 6

--- python-inheritance/6-rectangle.py
+++ python-inheritance/6-rectangle.py
@@ -1,58 +1,23 @@
-"""defining a class BaseGeometry"""
-class BaseGeometry:
-    """a base class for geometrical operations"""
-    def area(self):
-        """calculates area of the geometry
-        raises Exception with the message "area() is not implemented"
-        """
-        raise Exception("area() is not implemented")
-    
-    def integer_validator(self, name, value):
-        """
-        Validate if the value is an integer greater than 0.
 
-        name (str): The name of the value being validated.
-        value (int): The value to be validated.
+"""Creates a Rectangle class."""
+BaseGeometry = __import__('5-base_geometry').BaseGeometry
 
-        Raises:
-            TypeError: If value is not an integer.
-            ValueError: If value is less or equal to 0.
-        """
-        if not isinstance(value, int):
-            raise TypeError("{} must be an integer" .format(name))
-        if value <= 0:
-            raise ValueError("{} must be greater than 0" .format(name))
-        
 class Rectangle(BaseGeometry):
+    """class Rectangle that inherits from BaseGeometry (7-base_geometry.py).
+    Private instance attributes:
+        - width
+        - height
+    Inherits from BaseGeometry.
     """
-    class Rectangle inherits from class BaseGeometry.
 
-    __width (int): The width of the rectangle.
-    __height (int): The height of the rectangle.
-
-    __init__(self, width, height): Initializes a Rectangle object with the specified width and height.
-    __str__(self): Returns a string representation of the Rectangle object.
-    """
     def __init__(self, width, height):
+        """Initializes an instance.
+        Args:
+            - width: width of the rectangle
+            - heigth: height of the rectangle
         """
-        Initialize a Rectangle object with the specified width and height.
-
-        width (int): The width of the rectangle.
-        height (int): The height of the rectangle.
-
-        Raises:
-            TypeError: If width or height is not an integer.
-            ValueError: If width or height is less or equal to 0.
-        """
+        self.integer_validator("width", width)
+        self.integer_validator("height", height)
         self.__width = width
         self.__height = height
-        self.integer_validator("width", self.__width)
-        self.integer_validator("height", self.__height)
-
-    def __str__(self):
-        """
-        Returns a string representation of the Rectangle object.
-
-        Returns a string containing the width and height of the rectangle.
-        """
-        return f"[Rectangle] {self.__width}/{self.__height}"
+        
