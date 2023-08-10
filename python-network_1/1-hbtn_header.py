89d41e39c8d676d808822ecb7762e0036cdf587c
Aderynlola <125492983+Aderynlola@users.noreply.github.com>
2023-08-10 14:32:48 +0100

    Task 1

--- python-network_1/1-hbtn_header.py
+++ python-network_1/1-hbtn_header.py
@@ -1,21 +1,17 @@
-"""Fetches the value of the X-Request-Id header from a given URL's response"""
+"""
+Takes in a URL, sends a request, and displays the value of the variable X-Request-Id in the response header
+"""
 
 import requests
 import sys
 
-def main():
-    """Main function to fetch and display the X-Request-Id header"""
+if __name__ == "__main__":
     if len(sys.argv) != 2:
-        print("Usage: ./1-hbtn_header.py <URL>")
-        sys.exit(1)
+        sys.exit("Usage: ./1-hbtn_header.py <URL>")
 
     url = sys.argv[1]
     response = requests.get(url)
     x_request_id = response.headers.get('X-Request-Id')
 
-    if x_request_id is not None:
-        print(x_request_id)
-
-if __name__ == "__main__":
-    main()
+    print(x_request_id)
     
