b81f1ffbb4829101492b8edbf30d45ccd58eb816
Aderynlola <125492983+Aderynlola@users.noreply.github.com>
2023-10-09 11:57:55 +0100

    Task 1

--- api/1-export_to_CSV.py
+++ api/1-export_to_CSV.py
@@ -23,3 +23,4 @@ if __name__ == "__main__":
         header = ["user_ID", "username", "completed", "task"]
         writer = DictWriter(f, fieldnames=header, quoting=QUOTE_ALL)
         writer.writerows(todo_list)
+        
